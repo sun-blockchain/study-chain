29135a6e46b1c163c7199b6996a371d341491114
tantv-918 <trinh.van.tan@sun-asterisk.com>
2020-02-28 14:58:56 +0700

    Release

--- server/cli/init.sh
+++ server/cli/init.sh
@@ -8,12 +8,12 @@ sleep 3
 node invoke.js --username=adminacademy --func=CreateCourse --courseCode=BC01 --courseName=Blockchain --description="Blockchain Basic, you will learn about architech of blockchain, consensus,..." --shortDescription=Blockchain
 node invoke.js --username=adminacademy --func=CreateSubject --subjectCode=ET01 --subjectName=Ethereum --shortDescription=Ethereum --description="Ethereum basic: you will be learnt about solidity, EVM and architech of Ethereum Blockchain"
 
-sleep 1
+sleep 3
 
 node query.js --username=adminacademy --func=GetAllCourses
 node query.js --username=adminacademy --func=GetAllSubjects
 
-sleep 15
+sleep 3
 
 # node invoke.js --username=adminacademy --func=AddSubjectToCourse --courseId=xxxx --subjectId=xxx
 # node invoke.js --username=adminacademy --func=CreateClass --classCode=ETH101 --room=F13 --time="11:00" --startDate="20/02/2020" --endDate="20/05/2020" --repeat="Weekly" --shortDescription=Ethereum --description="Sun Research Blockchain will teach you knowledge about Ethereum. After class, you can develop a Dapp on Ethereum" --subjectId= --capacity=75
