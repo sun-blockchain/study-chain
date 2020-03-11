48886b2d8fcf3da60c9d5ebe89354c3d8aabb226
tan <lop12a1trinhvantan@gmail.com>
2020-03-11 12:46:29 +0700

    Auto deploy

--- network/upgrade_chaincode.sh
+++ network/upgrade_chaincode.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-export $(cat .env | xargs)
-docker restart cli
-sleep 5
-docker exec cli bash scripts/script_upgrade_chaincode.sh $CHAINCODE_NEW_VERSION
+export $(cat .envchaincode | xargs)
+NEW_CHECK_SUM=$(find ../chaincode/academy/go/  -type f | sort -u | xargs cat | md5sum)
+NEW_CHECK_SUM=${NEW_CHECK_SUM:0:32}
+if [[ $CHECK_SUM != $NEW_CHECK_SUM ]]
+then
+    NEW_VERSION=$(($CHAINCODE_VERSION + 1))
+    printf "CHAINCODE_VERSION=${NEW_VERSION}\nCHECK_SUM=${NEW_CHECK_SUM}" > .envchaincode
+    docker restart cli
+    sleep 5
+    docker exec cli bash scripts/script_upgrade_chaincode.sh $NEW_VERSION
+fi 
